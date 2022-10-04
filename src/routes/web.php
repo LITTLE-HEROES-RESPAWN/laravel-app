9b9a97e579f11a8fdbfcb8a1dca406c3cacf1ec6
katsuya_fukunaga <katsuya_fukunaga@respawn.jp>
2022-10-04 22:26:26 +0900

    breeze インストール

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,5 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', [\App\Http\Controllers\TestController::class, 'test'])->name('test');
-Route::get('now', [\App\Http\Controllers\TestController::class, 'now'])->name('now');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
