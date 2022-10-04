4e85ccd447ba7d7616a0ec5452aac31c3f6d025f
katsuya_fukunaga <katsuya_fukunaga@respawn.jp>
2022-10-04 17:24:14 +0900

    レイアウト共通化

---
+++ src/resources/views/components/card.blade.php
@@ -0,0 +1,13 @@
+<div class="card neumorphism elevation-1 neu-flat">
+    <div class="card-header">
+        <h1 class="card-header-title">
+            {{ $title }}
+        </h1>
+    </div>
+    <div class="card-body">
+        {{ $slot }}
+    </div>
+    <div class="card-actions">
+        {{ $actions }}
+    </div>
+</div>
