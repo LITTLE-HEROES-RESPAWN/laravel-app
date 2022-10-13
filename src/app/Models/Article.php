8af162c44e34a602e6709f14a36172bc0be69008
katsuya_fukunaga <katsuya_fukunaga@respawn.jp>
2022-10-13 20:16:23 +0900

    記事テーブル・モデル作成

---
+++ src/app/Models/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'content'];
+
+    /**
+     * Get the user that owns the Article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
