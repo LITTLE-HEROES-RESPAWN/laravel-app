1d5ab722a2134155d0ebcd0b31e810e7b41f8eca
katsuya_fukunaga <katsuya_fukunaga@respawn.jp>
2022-10-14 18:14:31 +0900

    Controller・Validation

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
 {
@@ -34,7 +36,27 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        // バリデータの作成
+        $validator = Validator::make($request->all(), [
+            'title' => ['required', 'string', 'max:5'],
+            'content' => 'required|string|max:5000',
+        ], customAttributes:[
+            'content' => '記事内容'
+        ]);
+        // バリデーション & 確認済みのデータ
+        $data = $validator->validate();
+
+        // ユーザー入力値を代入（Articleの「$fillable」に定義された値のみ代入される）
+        $article = Article::make($data);
+        // 送信ユーザーを取得
+        $user = $request->user();
+        // 記事の作成者を$userに紐付ける
+        $article->user()->associate($user);
+        // 保存
+        $article->save();
+
+        // ダッシュボードに遷移
+        return redirect()->route('dashboard');
     }
 
     /**
--- src/resources/views/articles/create.blade.php
+++ src/resources/views/articles/create.blade.php
@@ -9,18 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
                     <p class="mb-4 font-bold text-lg">記事投稿</p>
+
                     <form action="{{ route('articles.store') }}" method="post">
                         @csrf
                         <div class="pb-6">
                             <x-input-label for="article-title" value="タイトル" />
                             <x-text-input id="article-title" class="block mt-1 w-full" type="text" name="title"
-                                required />
+                                required value="{{ old('title') }}" />
+                            <x-input-error :messages="$errors->get('title')" />
                         </div>
                         <div class="pb-6">
                             <x-input-label for="article-content" value="記事内容" />
                             <x-textarea id="article-content" class="block mt-1 w-full" type="textarea" name="content"
-                                required rows="10"></x-textarea>
+                                required rows="10">{{ old('content') }}</x-textarea>
+                            <x-input-error :messages="$errors->get('content')" />
                         </div>
                         <div class="flex">
                             <x-primary-button class="ml-auto justify-center w-full md:w-auto" type="submit">送信
