7eabe06c0878de2a34e76a051cb135537bf3c10b
katsuya_fukunaga <katsuya_fukunaga@respawn.jp>
2022-10-14 17:32:28 +0900

    作成画面

--- src/resources/views/articles/create.blade.php
+++ src/resources/views/articles/create.blade.php
@@ -9,7 +9,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    記事作成画面
+                    <p class="mb-4 font-bold text-lg">記事投稿</p>
+                    <form action="{{ route('articles.store') }}" method="post">
+                        @csrf
+                        <div class="pb-6">
+                            <x-input-label for="article-title" value="タイトル" />
+                            <x-text-input id="article-title" class="block mt-1 w-full" type="text" name="title"
+                                required />
+                        </div>
+                        <div class="pb-6">
+                            <x-input-label for="article-content" value="記事内容" />
+                            <x-textarea id="article-content" class="block mt-1 w-full" type="textarea" name="content"
+                                required rows="10"></x-textarea>
+                        </div>
+                        <div class="flex">
+                            <x-primary-button class="ml-auto justify-center w-full md:w-auto" type="submit">送信
+                            </x-primary-button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
